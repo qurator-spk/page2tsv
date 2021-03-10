243d3739134db160abbcb6006f6301517cecd2e6
Clemens Neudecker <952378+cneud@users.noreply.github.com>
2021-03-10 13:43:39 +0100

    fix iiif-url (dirty hack)

--- tsvtools/cli.py
+++ tsvtools/cli.py
@@ -273,6 +273,6 @@ def make_page2tsv_commands(xls_file, directory, purpose):
             if ma:
                 print('page2tsv {} {}.tsv '
                       '--image-url=https://content.staatsbibliothek-berlin.de/dc/'
-                      '{}-{}/left,top,width,height/full/1200/default.jpg --scale-factor=1.0 --purpose={}'.
+                      '{}-0000{}/left,top,width,height/1200,/0/default.jpg --scale-factor=1.0 --purpose={}'.
                       format(file, ma.group(1), ma.group(2), ma.group(3), purpose))
 
